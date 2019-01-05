ddafa30909e7671678d2769e30addc1e194b9ec7
Ryandw11 <>
2019-01-05 15:14:11 -0700

    Initial commit

---
+++ src/me/ryandw11/fun/Fun.java
@@ -0,0 +1,32 @@
+package me.ryandw11.fun;
+
+import javax.swing.JFrame;
+
+public class Fun {
+
+	public static void main(String[] args) {
+		String buttion1 = "a";
+		String buttion2 = "s";
+		if(args.length > 1) {
+			if(args[0] == null) {
+				buttion1 = "a";
+			}
+			if(args[1] == null) {
+				buttion2 = "s";
+			}
+			if(args[0] != null && args[1] != null) {
+				
+			buttion1 = args[0];
+			buttion2 = args[1];
+			}
+			
+		}else {
+			
+		}
+		
+		Window w = new Window(buttion1, buttion2);
+		w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+	}
+
+}
---
+++ src/me/ryandw11/fun/Window.java
@@ -0,0 +1,58 @@
+package me.ryandw11.fun;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Window extends JFrame {
+	
+	public int counter = 0;
+	JLabel label;
+	
+	public Window(String buttion1, String buttion2) {
+		super("Counter");
+		
+		
+		this.setSize(100, 100);
+		
+		this.guiConstructor();
+		
+		this.setVisible(true);
+		this.setLocation(700, 200);
+		this.getContentPane().setBackground(Color.green);
+		
+		this.addKeyListener(new KeyListener() {
+	        @Override
+	        public void keyTyped(KeyEvent e) {
+	        }
+
+	        @Override
+	        public void keyPressed(KeyEvent e) {
+	            System.out.println("Key pressed code=" + e.getKeyCode() + ", char=" + e.getKeyChar());
+	            if(e.getKeyChar() == buttion1.charAt(0)) {
+	            	counter += 1;
+	            	label.setText(counter + "");
+	            }
+	            if(e.getKeyChar() == buttion2.charAt(0)) {
+	            	counter -= 1;
+	            	label.setText(counter + "");
+	            }
+	        }
+
+	        @Override
+	        public void keyReleased(KeyEvent e) {
+	        }
+	    });	
+	}
+	
+	public void guiConstructor(){
+		label = new JLabel("0");
+		label.setFont(new Font("Arial", Font.PLAIN, 80));
+		label.setForeground(Color.white);
+		this.add(label);
+	}
+}
